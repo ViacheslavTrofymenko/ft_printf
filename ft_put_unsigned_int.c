7ddfe78600c503aa4cfcbc181017186ec6511ef1
ViacheslavTrofymenko <trofymenko.v.n@gmail.com>
2025-04-27 21:37:50 +0200

    Added function ft_put_unsigned_int

--- ft_put_unsigned_int.c
+++ ft_put_unsigned_int.c
@@ -3,52 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_put_unsigned_int.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vtrofyme <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: viacheslav <viacheslav@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 11:53:11 by vtrofyme          #+#    #+#             */
-/*   Updated: 2025/04/27 11:53:13 by vtrofyme         ###   ########.fr       */
+/*   Updated: 2025/04/27 21:36:02 by viacheslav       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_put_unsigned_int(unsigned int n)
+static void	ft_print_unsigned(unsigned int nbr)
 {
-	int		count;
+	if (nbr > 9)
+		ft_print_unsigned(nbr / 10);
+	ft_putchar_fd((nbr % 10) + '0', 1);
+}
 
-	printout(n);
+int	ft_put_unsigned_int(unsigned int nbr)
+{
+	int count;
+
 	count = 1;
-	while (n > 9)
+	while (nbr > 9)
 	{
-		n = n / 10;
+		nbr = nbr / 10;
 		count++;
 	}
+	ft_print_unsigned(nbr);
 	return (count);
-}
-
-
-static void	printout(unsigned int nb)
-{
-	if (nb > 9)
-		print_unsigned(nb / 10);
-	if (nb <= 9)
-	{
-		ft_putchar_fd(nb + 48, 1);
-		return ;
-	}
-	ft_putchar_fd((nb % 10) + 48, 1);
-}
-
-int	print_unsigned(unsigned int nb)
-{
-	unsigned int	i;
-
-	printout(nb);
-	i = 1;
-	while (nb > 9)
-	{
-		nb = nb / 10;
-		i++;
-	}
-	return (i);
 }
